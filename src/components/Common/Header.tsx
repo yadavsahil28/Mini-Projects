735eef2eb2dd2a34e6cfea7a130bc1c52d1605b2
Sahil Yadav <sahil2804yadav@gmail.com>
2026-04-07 08:19:25 +0530

    Updated design for header

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -4,7 +4,8 @@ type HeaderProps = {
 
 const Header = ({ title }: HeaderProps) => {
   return (
-    <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center font-bold">
+    <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center font-bold py-4 border-b
+                    border-gray-200 shadow-sm bg-white">
       {title}
     </div>
   )
